a554e8b8c5195ecaae27015d4c408162adb56457
Heliozoa <dm89132@gmail.com>
2020-02-10 22:29:01 +0200

    eslint

--- index.js
+++ index.js
@@ -1,101 +1,101 @@
-require("dotenv").config()
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
-const Person = require("./models/person")
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
 const mongoose = require('mongoose')
 
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use(express.static("build"))
+app.use(express.static('build'))
 app.use(morgan(function (tokens, req, res) {
-    let arr = [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, "content-length"), "-",
-        tokens["response-time"](req, res), "ms"
-    ]
-    if (req.method === "POST") {
-        const data = req.body
-        arr.push(JSON.stringify(data))
-    }
-    return arr.join(" ")
+  let arr = [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ]
+  if (req.method === 'POST') {
+    const data = req.body
+    arr.push(JSON.stringify(data))
+  }
+  return arr.join(' ')
 }))
 
 const url = process.env.MONGODB_URI
 mongoose.set('useFindAndModify', false)
-mongoose.set('useCreateIndex', true);
+mongoose.set('useCreateIndex', true)
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(res => {
-        console.log('connected to MongoDB')
-    })
-    .catch(e => console.error('error connecting to MongoDB:', e.message))
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(e => console.error('error connecting to MongoDB:', e.message))
 
 
-app.get("/", (req, res, next) => {
-    res.send("<h1>Hello</h1>")
+app.get('/', (req, res) => {
+  res.send('<h1>Hello</h1>')
 })
 
-app.get("/info", (req, res, next) => {
-    Person.find({}).then(persons => {
-        const count = persons.length
-        const date = Date()
-        res.send(`Phonebook has info for ${count} people<br><br>${date}`)
+app.get('/info', (req, res, next) => {
+  Person.find({}).then(persons => {
+    const count = persons.length
+    const date = Date()
+    res.send(`Phonebook has info for ${count} people<br><br>${date}`)
+  }).catch(e => next(e))
+})
+
+app.get('/api/persons', (req, res, next) => {
+  Person.find({}).then(persons => {
+    res.json(persons.map(p => p.toJSON()))
+  }).catch(e => next(e))
+})
+
+app.get('/api/persons/:id', (req, res, next) => {
+  const id = String(req.params.id)
+  Person.find({ _id: id })
+    .then(person => {
+      console.log('found', person)
+      res.send(person)
     }).catch(e => next(e))
 })
 
-app.get("/api/persons", (req, res, next) => {
-    Person.find({}).then(persons => {
-        res.json(persons.map(p => p.toJSON()))
+app.delete('/api/persons/:id', (req, res, next) => {
+  const id = String(req.params.id)
+  Person.findOne({ _id: id })
+    .then(p => {
+      p.delete()
+      res.send(p)
+    })
+    .catch(e => next(e))
+})
+
+app.post('/api/persons', (req, res, next) => {
+  const body = req.body
+  const name = body.name
+  const number = body.number
+  const person = new Person({
+    name,
+    number,
+  })
+  person.save().then(p => res.json(p.toJSON())).catch(e => next(e))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const id = String(req.params.id)
+  const person = req.body
+  console.log('updating', person, id)
+  Person.findOneAndUpdate({ _id: id }, person, { runValidators: true, context: 'query' })
+    .then(old => {
+      console.log('updated', old, 'to', person)
+      res.json(person)
     }).catch(e => next(e))
 })
 
-app.get("/api/persons/:id", (req, res, next) => {
-    const id = String(req.params.id)
-    Person.find({ _id: id })
-        .then(person => {
-            console.log("found", person)
-            res.send(person)
-        }).catch(e => next(e))
-})
-
-app.delete("/api/persons/:id", (req, res, next) => {
-    const id = String(req.params.id)
-    Person.findOne({ _id: id })
-        .then(p => {
-            p.delete()
-            res.send(p)
-        })
-        .catch(e => next(e))
-})
-
-app.post("/api/persons", (req, res, next) => {
-    const body = req.body
-    const name = body.name
-    const number = body.number
-    const person = new Person({
-        name,
-        number,
-    })
-    person.save().then(p => res.json(p.toJSON())).catch(e => next(e))
-})
-
-app.put("/api/persons/:id", (req, res, next) => {
-    const id = String(req.params.id)
-    const person = req.body
-    console.log("updating", person, id)
-    Person.findOneAndUpdate({ _id: id }, person, { runValidators: true, context: 'query' })
-        .then(old => {
-            console.log("updated", old, "to", person)
-            res.json(person)
-        }).catch(e => next(e))
-})
-
-const errorHandler = (err, req, res, next) => {
-    console.error(err.name, ":", err.message)
-    return res.status(500).send({ error: err.message })
+const errorHandler = (err, req, res) => {
+  console.error(err.name, ':', err.message)
+  return res.status(500).send({ error: err.message })
 }
 app.use(errorHandler)
 
--- models/person.js
+++ models/person.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator');
+const uniqueValidator = require('mongoose-unique-validator')
 
 const schema = new mongoose.Schema({
-    name: { type: String, required: true, minlength: 3, unique: true },
-    number: { type: String, required: true, minlength: 8, unique: false },
+  name: { type: String, required: true, minlength: 3, unique: true },
+  number: { type: String, required: true, minlength: 8, unique: false },
 })
 
 schema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
-schema.plugin(uniqueValidator);
+schema.plugin(uniqueValidator)
 
 const Person = mongoose.model('Person', schema)
 
